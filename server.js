6fb7a001c04a7211f274418f4b19136228029b36
Alex Glaubitz <e.pirazzi@gmail.com>
2022-05-03 15:25:51 -0400

    username and password hidden using dotenv

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
-const res = require('express/lib/response');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+// required to use .env file to hide username, password, and database name - npm install dotenv --save
+require('dotenv').config();
 
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+ // connect to database
+ const db = mysql.createConnection(
+   {
+     host: 'localhost',
+     // mysql username
+     user: process.env.DB_USER,
+     // mysql pass
+     password: process.env.DB_PASSWORD,
+     database: process.env.DB_NAME
+   },
+   console.log('Connected to the election database.')
+ );
 
 // default response for any other request (not found)
 // place at bottom of routes to avoid overwriting a valid route
